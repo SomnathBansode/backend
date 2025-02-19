d1cb2b05c97e9773ae99fde18df7bfbadb4a9d9d
SomnathBansode <bansodesomnath888@gmail.com>
2025-02-19 15:32:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/user', (req, res) => {
+  res.send('Hello user!')
+})
+
+app.get('/user/:user', (req, res) => {
+  const user = req.params.user
+  res.send(`Hello ${user}!`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
